19e236256b131e4c2df81a5dc70b18729814c4f4
luckygro <lukas@luckyg.de>
2019-12-14 01:07:42 +0100

    add node/sass

--- pages/news.tsx
+++ pages/news.tsx
@@ -7,7 +7,7 @@ import { Entry , EntryCollection} from 'contentful'
 import {INewsEntry, IPageContent} from '../interfaces/contentDelivery'
 
 import {ArticleThumb } from '../components/news';
-import { MiscButton, ParseJSON } from '../components/misc';
+import { ParseJSON } from '../components/misc';
 
 // import eyecatcher from '/images/schoolchildren.jpg'
 
@@ -111,44 +111,153 @@ class NewsPage extends React.Component<IProps, IState> {
               </section>
             </div>
           </div> 
+
+        <style global jsx>{`
+        body {
+          margin: 0;
+        }
+        @import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
+
+        h1, h2, h3, span, a, p, button, input {
+          font-family: Roboto;
+        }
+
+        h1 {
+          color: #ccd42c;
+          font-size: 46pt;
+        }
+
+        h2 {
+          color: #aaa;
+          font-size: 28pt;
+        }
+
+        h3 {
+          color: #666;
+        }
+
+        input, textarea {
+          outline: none;
+          border: 0px solid transparent;
+          padding: 10px 15px;
+          background-color: transparent;
+          font-size: 12pt;
+          border: 2px #bbb solid;
+          border-radius: 20px;
+          color: #666;
+          margin-left: 1em;
+          transition: all 0.3s;
+
+          &::placeholder {
+            color: #aaa;
+          }
+        }
+
+        button {
+          outline: none;
+          border: 0px solid transparent;
+          padding: 10px 15px;
+          background-color: transparent;
+          font-size: 14pt;
+          border: 2px #bbb solid;
+          border-radius: 20px;
+          color: #bbb;
+          text-transform: uppercase;
+          margin-left: 1em;
+          transition: all 0.3s;
+          cursor: pointer;
+          
+          :hover {
+            color: #999;
+            border-color: #999;
+          }
+
+          &.inverse {
+            background-color: #bbb;
+            color: #fff;
+            border-color: transparent;
+              
+            :hover {
+              background-color: #999;
+            }
+          }
+
+          &.priority {
+            background-color: #ccd42c;
+            color: #fff;
+            border-color: transparent;
+          
+            :hover {
+              background-color: #7d8418;
+            }
+          }
+        }        
+
+        .logo {
+          height: 80px;
+        }
+
+
+        `}</style>
+
+        <style jsx>{`
+
+        /* ================= */
+        /* ==== LAYOUT ===== */
+
+        section {
+          padding: 60px 0px;
+          width: 100%;
+          display: flex;
+          justify-content: center;
+
+          > div {
+            width: 90%;
+            max-width: 1200px;
+          }
+        }
+
+        /* Eyecatcher */
+
+        .eyecatcher {
+          width: 100%;
+          grid-template-columns: 2fr 1fr;
+          display: grid;
+          height: 500px;
+        }
+
+        .eyecatchercontent {
+          display: flex;
+          text-align: right;
+          align-items: center;
+          padding: 60px;
+        }
+
+        .eyecatchercontent div {
+          width: 100%;
+        }
+
+        .eyecatcherimage {
+          display: flex;
+          justify-content: left;
+          /* background-color: #ccd42c;*/
+          align-items: center;
+        }
+
+        /* Introduction */
+
+        .introduction {
+          background-color: #bbb;
+
+          h2 {
+            color: #fff;
+          }
+        }
+
+        `}</style>
       </Layout>
     );
   }
 }
 
 export default withContentful(NewsPage);
-
-
-
-// export const pageQuery = graphql`
-//   query newsPageQuery {
-//     feed: allContentfulNews(sort: {fields: publishDate, order: DESC}, limit: 5) {
-//       edges {
-//         node {
-//           title
-//           slug
-//           publishDate
-//           heroImage {
-//             fixed(width: 300) {
-//               width
-//               height
-//               src
-//               srcSet
-//             }
-//             title
-//           }
-//           summary {
-//             summary
-//           }
-//         }
-//       }
-//     }
-//     page: contentfulPage(contentful_id: {eq: "5DGQAcf8cNd5ThMX95NlfY"}) {
-//       title
-//       content {
-//         json
-//       }
-//     }
-//   }
-// `
-
