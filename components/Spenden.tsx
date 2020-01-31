9929d24ed9586ace21e01d34fea90a153eb3ec24
luckygro <lukas@luckyg.de>
2020-01-31 16:58:38 +0100

    Textänderungen im Bereich Spenden

--- components/Spenden.tsx
+++ components/Spenden.tsx
@@ -5,8 +5,8 @@ import { MiscButton } from './Misc';
 export default () => (
   	<section id="Spenden" className="sidebar">
 		<h3>Spenden</h3>
-		<p>Sie möchten Bischof Gabriel Kijjambu finanziell unterstützen? Wir freuen uns auf eine Spende auf folgende Kontoverbindung:</p>
-		<span>IBAN: DE24 6035 0130 0001 0955 26<br />BIC BBKRDE6BXXX</span>
+		<p>Sie möchten die Arbeit von Bischof Gabriel Kijjambu finanziell unterstützen? Wir freuen uns über eine Spende auf folgende Kontoverbindung:</p>
+		<span>Empfänger: CKLUganda<br />IBAN: DE24 6035 0130 0001 0955 26<br />BIC BBKRDE6BXXX</span>
 		<p>Gerne stellen wir Ihnen eine Spendenbescheinigung aus. Bitte kontaktieren Sie uns:</p>
 		<MiscButton link="/contact" cssclass="button button-primary" text="Kontakt" />
 	</section>
